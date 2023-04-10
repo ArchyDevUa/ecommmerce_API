bbaa0ea3e438ff2669a52e21735f021a4a0b90af
Archy <archydevua@gmail.com>
2023-04-10 17:49:34 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+﻿const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(()=> console.log("DB Connection Success"))
+    .catch((err) => console.log(err));
+app.listen(process.env.PORT || 5000,()=>{
+    console.log("Server is running")
+});
